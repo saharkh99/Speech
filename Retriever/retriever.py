4055b16265373355383d59d3f9cdd35df2cada4f
Saharkh99 <sahar.khazali.bte@gmail.com>
2024-06-20 12:32:03 -0600

    next_one

---
+++ Retriever/retriever.py
@@ -0,0 +1,5 @@
+# Fusion retrieval or hybrid search
+# reranking
+# different similarity metrics 
+# Search Module — in addition to retrieving context from vector database, search modules intergrates data from other sources such as search engines, tabular data, knowledge graphs etc.
+# Fusion — involves parallel vector searches of both original and expanded queries, intelligent reranking to optimize results, and pairing the best outcomes with new queries.
