e8d044964f29fe6b90f285d46702e16dcaf6302d
Saharkh99 <sahar.khazali.bte@gmail.com>
2024-06-21 08:17:18 -0600

    ranking

--- Retriever/ranking.py
+++ Retriever/ranking.py
@@ -1,11 +1,26 @@
 import langchain
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import FlashrankRerank
 
 
-
-def ensembler(pinecone_retriever, elastic_retriever, user_query):
+def ensembler(pinecone_retriever, elastic_retriever, user_query,k):
     ensemble_retriever = langchain.EnsembleRetriever([pinecone_retriever, elastic_retriever], weights=[0.5, 0.5])
-    retrieved_data = ensemble_retriever.retrieve(user_query)
-    return retrieved_data
+    config = {"configurable": {"search_kwargs_faiss": {"k": k}}}
+    docs = ensemble_retriever.invoke(user_query, config=config)
+    return docs
+
+def flashrank(retriever,query):
+   compressor = FlashrankRerank()
+   compression_retriever = ContextualCompressionRetriever(
+        base_compressor=compressor, base_retriever=retriever
+    )
+
+   compressed_docs = compression_retriever.invoke(
+        query
+    )
+   
+   return compressed_docs
+
 
 
 
---
+++ query/querry_processing.py
@@ -0,0 +1,3 @@
+# Step-back prompting uses LLM to generate a more general query
+# Query re-writing uses LLM to reformulate initial query
+# Query Translation and transformation 
